2105f850108545e97903b6ddef25249a4c2760a7
“sujitmemane” <officialsujitmemane@gmail.comgit config --global user.name sujitmemanegit config --global user.email officialsujitmemane@gmail.com>
2023-12-13 11:09:05 +0530

    controllers updated

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,46 +1,48 @@
-import User from "../models/user.js"
-import jwt from "jsonwebtoken"
+import User from "../models/user.js";
+import jwt from "jsonwebtoken";
 
-const createToken = (_id)=>{
-  return  jwt.sign({_id},process.env.JWT_SECRET,{expiresIn:'3d'})
-}
-
-
-
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+};
 
 export const loginUser = async (req, res) => {
-    const {email, password} = req.body
-  
-    try {
-      const user = await User.login(email, password)
-  
-      
-      const token = createToken(user._id)
-  
-      res.json({success:true, message:"Login Successfull",user:{token:token,username:user?.name,user_id:user?._id}})
-    } catch (error) {
-        res.status(404).json({success:false,message:error?.message})
-    }
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    const token = createToken(user._id);
+
+    res.json({
+      success: true,
+      message: "Login Successful",
+      user: { token, username: user?.name, user_id: user?._id },
+    });
+  } catch (error) {
+    res.status(401).json({ success: false, message: error?.message });
   }
+};
 
-export const registerUser = async(req,res)=>{
-    const {name,email,password} = req.body
-    try {
-        const user = await User.register(name,email,password)
-        const token = createToken(user?.id)
-        res.json({success:true,message:"Registeration Successfull",user:{token:token,username:user?.name,user_id:user?.id}})
-    } catch (error) {
-        res.status(404).json({success:false,message:error?.message})
-    }
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = await User.register(name, email, password);
+    const token = createToken(user?.id);
+    res.json({
+      success: true,
+      message: "Registration Successful",
+      user: { token, username: user?.name, user_id: user?.id },
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error?.message });
+  }
+};
 
-}
-
-
-export const getAllUsers = async(req,res)=>{
-    try {
-        const users = await User.find()
-        res.json({success:true,message:"All Users",users:users})
-    } catch (error) {
-        res.json({success:false,message:error?.message})
-    }
-}
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json({ success: true, message: "All Users", users: users });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error?.message });
+  }
+};
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,19 +1,54 @@
-import jwt from "jsonwebtoken"
-import User from "../models/user.js"
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
 
-export const authMiddleware = async(req,res,next)=>{
-    const {authorization} = req.headers
-    if(!authorization){
-        res.json({success:false,message:"Please Login or Register"})
-    }
-    const token = authorization.split(" ")[1]
-    try {
-        const {_id} = jwt.verify(token,process.env.JWT_SECRET)
-        req.user = await User.findOne({_id:_id}).select({_id})
-        next()
-    } catch (error) {
-        res.status(400).json({success:false,message:"Request is not Authorized"})
+export const  authMiddleware = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized: Please provide a valid token",
+      });
     }
 
-  
-}
+    const token = authorization.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized: Token is missing",
+      });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decodedToken) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized: Invalid token",
+      });
+    }
+
+    const { _id } = decodedToken;
+    const user = await User.findOne({ _id }).select({ _id });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized: User not found",
+      });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Authentication Error:", error);
+    res.status(401).json({
+      success: false,
+      message: "Unauthorized: Invalid token or authentication error",
+    });
+  }
+};
+
+
